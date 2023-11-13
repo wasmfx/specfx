8d23a34a95e8f993bb8fcdc701c532e1d749bee4
dhil <dhil@users.noreply.github.com>
2023-11-13 08:30:44 +0000

    deploy: ab570e6e658e02f1c95be642c4f65fe07af9590b

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 + function-references + gc + tail-calls + continuations (Draft 2023-10-20)',
+    VERSION: '2.0 + function-references + gc + tail-calls + continuations (Draft 2023-11-13)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
